0614f678aaf3943cd82ea918c2e03867392e7a7e
Alen <alen@abelium.eu>
2018-04-24 10:05:55 +0200

    Ulomek, dodatne metode

--- 04_Java/Ulomek/src/com/prog2/ulomek/GlavniProgram.java
+++ 04_Java/Ulomek/src/com/prog2/ulomek/GlavniProgram.java
@@ -1,10 +1,29 @@
 package com.prog2.ulomek;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class GlavniProgram {
 
     public static void main(String[] args) {
         Ulomek u = new Ulomek(2, 3);
-        System.out.println(u);
+        Ulomek v = new Ulomek(6, 4);
+        System.out.println("na zacetku:" + u + " ... " + v);
+        Ulomek rezultat = u.vsota(v);
+        System.out.println(rezultat);
+        
+        System.out.println(Ulomek.vsota2(u, v));
+        
+        System.out.println("prej:" + u + " ... " + v);
+        u.pristej(v);
+        System.out.println("potem:" + u + " ... " + v);
+        
+        List<Ulomek> ulomki = new ArrayList<Ulomek>();
+        ulomki.add(new Ulomek(1, 2));
+        ulomki.add(new Ulomek(1, 4));
+        ulomki.add(new Ulomek(1, 5));
+                
+        System.out.println(Ulomek.sestejSeznam(ulomki));
     }
 
 }
--- 04_Java/Ulomek/src/com/prog2/ulomek/Ulomek.java
+++ 04_Java/Ulomek/src/com/prog2/ulomek/Ulomek.java
@@ -1,5 +1,7 @@
 package com.prog2.ulomek;
 
+import java.util.List;
+
 public class Ulomek {
     private int stevec;
     private int imenovalec;
@@ -13,7 +15,51 @@ public class Ulomek {
         this.stevec = stevilo;
         this.imenovalec = 1;
     }
+    
+    public Ulomek(Ulomek u) {
+        this.stevec = u.stevec;
+        this.imenovalec = u.imenovalec;
+    }
 
+    public Ulomek vsota(Ulomek u) {
+        Ulomek x = new Ulomek(
+                 (this.stevec*u.imenovalec + this.imenovalec*u.stevec),
+                 (this.imenovalec*u.imenovalec)
+               );
+        x.okrajsaj();
+        return x;
+    }
+            
+    public static Ulomek vsota2(Ulomek u, Ulomek v) {
+        Ulomek vmesni = new Ulomek(u);
+        return vmesni.vsota(v);
+    }
+    
+    public void pristej(Ulomek u) {
+        this.stevec = this.stevec*u.imenovalec + this.imenovalec*u.stevec;
+        this.imenovalec = this.imenovalec*u.imenovalec;
+        this.okrajsaj();
+    }
+    
+    public void okrajsaj() {
+        int delitelj = gcd(this.stevec, this.imenovalec);
+        this.stevec = this.stevec / delitelj;
+        this.imenovalec = this.imenovalec / delitelj;       
+    }
+    
+    public static int gcd(int a, int b) {
+        if(b == 0) return a;
+        return gcd(b, a % b);
+    }
+    
+    public static Ulomek sestejSeznam(List<Ulomek> seznam) {
+        Ulomek vsota = new Ulomek(0);
+        for(Ulomek u: seznam) {
+            vsota.pristej(u);
+        }
+        return vsota;
+    }
+    
     @Override
     public String toString() {
         return stevec + "/" + imenovalec ;
