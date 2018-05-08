d8366ec9f4517c580176384cc8491b1dc0456c32
Alen <alen@abelium.eu>
2018-05-08 09:30:09 +0200

    Še malo vmesnikov

--- 04_Java/Vmesniki/src/Test.java
+++ 04_Java/Vmesniki/src/Test.java
@@ -1,3 +1,5 @@
+import java.util.LinkedList;
+import java.util.List;
 
 public class Test {
 
@@ -6,6 +8,17 @@ public class Test {
         PametniTelefon pametni = new PametniTelefon(10);
         navaden.poklici("Francelj");
         pametni.poklici("Jožica");
+        List<Telefon> telefoni = new LinkedList<Telefon>();
+        telefoni.add(navaden);
+        telefoni.add(pametni);
+        String sef = "Janez";
+        for(Telefon t: telefoni) {
+            t.poklici(sef);
+        }
+        Telefon tel = pametni;
+        NavadenTelefon tn = pametni;
+        NavadenTelefon tn2 = (NavadenTelefon)tel;
+        ((PametniTelefon)tel).slikaj("Milica");
     }
 
 }
