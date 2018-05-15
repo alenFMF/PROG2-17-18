46248b2160951789a35953e61f10ab77ae565ac6
Alen <alen@abelium.eu>
2018-05-15 09:32:32 +0200

    Komentarji, antialiasing

--- 04_Java/Risanje/src/Okno.java
+++ 04_Java/Risanje/src/Okno.java
@@ -5,14 +5,18 @@ import javax.swing.JFrame;
 public class Okno extends JFrame {
     private Platno platno;
     
-    public Okno() {
+
+    public Okno() {        
+        // Okna so dediči JFrame. V konstruktorju sestavimo vsebino okna tako, da v 'pane' 
+        // dodajamo komponente.                 
         super();    
         Container pane = this.getContentPane();
-        platno = new Platno();
+        platno = new Platno();   
         pane.add(platno);
     }
     
     public static void main(String[] args) {
+        // Osnovna inicializacija okna.
         JFrame okno = new Okno();
         okno.pack();
         okno.setVisible(true);       
--- 04_Java/Risanje/src/Platno.java
+++ 04_Java/Risanje/src/Platno.java
@@ -1,9 +1,17 @@
+import java.awt.BasicStroke;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.RenderingHints;
 
 import javax.swing.JPanel;
 
+/**
+ * Če želimo komponento platno, na katero lahko rišemo, naredimo dediča razreda JPanel. 
+ * Da nastavimo velikost platna predefiniramo (povozimo) metodo getPreferredSize.
+ * Rišemo v metodi paintComponent s pomočjo objekta Graphics, ki ga med delovanjem sam poda Swing.
+ */
 public class Platno extends JPanel {
     public Platno() {
         super();
@@ -16,8 +24,18 @@ public class Platno extends JPanel {
 
     @Override
     protected void paintComponent(Graphics g) {
-        super.paintComponent(g);
-        g.setColor(Color.red);
+        super.paintComponent(g);       // naj se nariše prej vse, kar zahteva "oče".
+        g.setColor(Color.red);         // nad rišemo z ustreznimi ukai.
         g.fillRect(10, 10, 50, 50);
+        g.setColor(Color.blue);
+        
+        // Za lepše in bolj natančno risanje uporabljamo metode iz Graphic
+        Graphics2D g2 = (Graphics2D)g;
+        // antialiasing za manj nazobčeno grafiko
+        g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING,
+                RenderingHints.VALUE_ANTIALIAS_ON);
+        g2.setStroke(new BasicStroke(3));
+        g.drawLine(50, 200, 250, 10);
+        // https://docs.oracle.com/javase/7/docs/api/java/awt/Graphics.html               
     }   
 }
